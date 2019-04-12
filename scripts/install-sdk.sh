70a893ef16a2151e72543a88de5896e3955f1b9c
rob boll <robertcboll@gmail.com>
2019-04-12 17:12:00 -0400

    t

--- scripts/install-sdk.sh
+++ scripts/install-sdk.sh
@@ -29,8 +29,10 @@ setup_gcsproxy
 
 test -x bin/cork || {
 	log "installing cork"
+	set -x
 	curl -fsSL -o bin/cork http://localhost:${GCSPROXY_PORT}/${TOOLS_BUCKET#gs://}/mantle-${MANTLE_VERSION}/cork
 	chmod +x bin/cork
+	set +x
 }
 
 test -x bin/plume || {
