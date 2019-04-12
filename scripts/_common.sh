6515a042f4e1bf753df1d9ec2c2612362246edb2
rob boll <robertcboll@gmail.com>
2019-04-12 18:19:30 -0400

    t

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ export TOOLS_BUCKET="${TOOLS_BUCKET:-gs://datadog-os-tools}"
 export BUILDS_BUCKET="${BUILDS_BUCKET:-gs://datadog-os-builds}"
 export RELEASE_BUCKET="${RELEASE_BUCKET:-gs://datadog-os-images}"
 
-export MANTLE_VERSION="v0.0.2"
+export MANTLE_VERSION="v0.0.0"
 
 export GCSPROXY_PORT="${GCSPROXY_PORT:-8090}"
 export COREOS_DEV_BUILDS="http://localhost:${GCSPROXY_PORT}/${BUILDS_BUCKET#gs://}"
